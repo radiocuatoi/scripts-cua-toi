e12865f032cde8c7ed9c23bed21ce0eaf11f81fb
dungviettran89 <dungviettran89@gmail.com>
2019-05-26 21:47:57 +0800

    Added base files for gae and heroku

---
+++ gradle/gae.groovy
@@ -0,0 +1,78 @@
+println '''To enable GAE support:
+- add googleAppEngine("projectName")
+- add the following config:
+buildscript {
+    dependencies {
+        classpath "com.google.cloud.tools:appengine-gradle-plugin:1.3.5"
+    }
+}
+'''
+ext.googleAppEngine = { gaeProject ->
+    println "Enabling GAE with project ${gaeProject}"
+    apply plugin: 'com.google.cloud.tools.appengine'
+    appengineDeploy.dependsOn test
+    appengineStage.dependsOn test
+
+    dependencies {
+        providedRuntime('org.springframework.boot:spring-boot-starter-tomcat')
+    }
+
+    appengine {
+        deploy {
+            project = gaeProject
+            stopPreviousVersion = true
+            promote = true
+            version = '1'
+        }
+    }
+
+    task appengineDockerBuild(type: Exec, dependsOn: 'build') {
+        group = 'cuatoi'
+        doFirst {
+            new File("$buildDir/Dockerfile").text = """FROM google/cloud-sdk:alpine
+RUN apk --no-cache add openjdk8 nss
+ENV JAVA_HOME /usr/lib/jvm/java-1.8-openjdk
+ENV PATH \$PATH:/usr/lib/jvm/java-1.8-openjdk/jre/bin:/usr/lib/jvm/java-1.8-openjdk/bin
+RUN gcloud components install app-engine-java
+RUN java -version && javac -version
+WORKDIR /app/
+
+ADD . /app/
+"""
+        }
+        commandLine 'docker', "build", "--pull", "-t", "$gaeProject:dev", "-f", "$buildDir/Dockerfile", "."
+    }
+    task appengineDockerRun(type: Exec, dependsOn: 'appengineDockerBuild') {
+        group = 'cuatoi'
+        commandLine "docker", "run", "--rm", "--network=host", "--sig-proxy=true",
+                "-e", "BUILD_CREDENTIALS=${buildCredentials}",
+                "-v=gcloud-java-gradle:/root/.gradle", "--name=$gaeProject-dev", "$gaeProject:dev",
+                "./gradlew", "clean", "appengineRun"
+    }
+    task appengineDockerDeploy(type: Exec, dependsOn: 'appengineDockerBuild') {
+        group = 'cuatoi'
+        doFirst {
+            new File("$buildDir/deploy.sh").text = "gcloud auth activate-service-account --key-file=src/main/appengine/deploy.json &&" +
+                    "./gradlew clean appengineDeploy"
+        }
+        commandLine 'docker', "run", "--rm", "--network=host", "--sig-proxy=true",
+                "-e", "BUILD_CREDENTIALS=${buildCredentials}",
+                "-v=gcloud-java-gradle:/root/.gradle",
+                "-v=$buildDir/deploy.sh:/app/deploy.sh",
+                "--name=$gaeProject-dev", "$gaeProject:dev",
+                "sh", "./deploy.sh"
+    }
+    task appengineDockerDeployCron(type: Exec, dependsOn: 'appengineDockerBuild') {
+        group = 'cuatoi'
+        doFirst {
+            new File("$buildDir/deploy.sh").text = "gcloud auth activate-service-account --key-file=src/main/appengine/deploy.json &&" +
+                    "./gradlew clean appengineDeployCron"
+        }
+        commandLine 'docker', "run", "--rm", "--network=host", "--sig-proxy=true",
+                "-e", "BUILD_CREDENTIALS=${buildCredentials}",
+                "-v=gcloud-java-gradle:/root/.gradle",
+                "-v=$buildDir/deploy.sh:/app/deploy.sh",
+                "--name=$gaeProject-dev", "$gaeProject:dev",
+                "sh", "./deploy.sh"
+    }
+}
---
+++ gradle/heroku.groovy
@@ -0,0 +1,109 @@
+println '''To enable Heroku support:
+- add heroku([[appName: "APP_NAME_1", email  : "EMAIL_1", apiKey : "API_KEY_1"],
+              [appName: "APP_NAME_2", email  : "EMAIL_2", apiKey : "API_KEY_2"]])
+'''
+ext.heroku = { List<Map> apps ->
+    task herokuDeploy(type: Exec, dependsOn: 'build') {
+        group = 'cuatoi'
+        doFirst {
+            new File("$buildDir/deploy.sh").text = '''
+set -e
+echo "Deploying $APP_NAME using $EMAIL"
+echo "" > ~/.netrc
+echo "machine api.heroku.com" >> ~/.netrc
+echo "  login $EMAIL" >> ~/.netrc
+echo "  password $API_KEY" >> ~/.netrc
+echo "machine git.heroku.com" >> ~/.netrc
+echo "  login $EMAIL" >> ~/.netrc
+echo "  password $API_KEY" >> ~/.netrc
+git config --global user.email "$EMAIL"
+git config --global user.name "$APP_NAME"
+git config --global push.default simple
+
+heroku repo:reset -a $APP_NAME
+
+rm -rf /git/
+mkdir /git/
+cd /git/
+git init
+heroku git:remote -a $APP_NAME 
+rm -rf *
+
+cp /staging/caddy ./
+cp /staging/build/pom.xml ./
+cp /staging/build/Caddyfile ./
+cp /staging/build/run.sh ./
+cp /staging/build/start.sh ./
+cp /staging/build/libs/* ./
+echo 'web: ./start.sh' > ./Procfile
+echo "export HEROKU_APP_NAME=$APP_NAME" > ./setenv.sh
+chmod +x ./setenv.sh
+echo $(date) > stamp.txt
+
+git add -A .
+git commit -m "OK $(date)"
+git push heroku master -f
+'''
+            new File("$buildDir/pom.xml").text = '''<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+    <modelVersion>4.0.0</modelVersion>
+    <groupId>groupId</groupId>
+    <artifactId>artifactid</artifactId>
+    <version>1.0-SNAPSHOT</version>
+    <packaging>jar</packaging>
+    <name>projectName</name>
+</project>
+'''
+            new File("$buildDir/Caddyfile").text = '''
+0.0.0.0
+tls off
+proxy / localhost:8080 {
+    transparent
+    websocket
+}
+'''
+            new File("$buildDir/run.sh").text = '''
+set -e
+export APP_PORT=8080
+java -Xss512k -Xms64m -Xmx256m -server\\
+    -Duser.timezone=Asia/Ho_Chi_Minh \\
+    -Dspring.profiles.active=default,prod \\
+    -jar *.jar
+'''
+            new File("$buildDir/start.sh").text = '''
+set -e
+echo "Starting heroku-deployer "
+if [ -z "$PORT" ]; then
+    echo "PORT parameter not found, setting to 8081"
+    export PORT=8081
+fi
+
+echo "Caddy Port is $PORT"
+./caddy -conf ./Caddyfile -port ${PORT} &
+echo "Caddy started."
+. ./setenv.sh
+echo "HEROKU_APP_NAME=$HEROKU_APP_NAME"
+echo "Starting app..."
+./run.sh
+'''
+
+            def deployCmd = ''
+            apps.forEach { app ->
+                deployCmd += "RUN EMAIL=${app.email} APP_NAME=${app.appName} API_KEY=${app.apiKey} ./build/deploy.sh\n"
+            }
+            new File("$buildDir/Dockerfile").text = '''FROM abiosoft/caddy
+WORKDIR /staging/
+#prepare cli and static
+RUN apk --no-cache add nodejs npm unzip
+RUN npm install -g heroku
+RUN heroku plugins:install heroku-repo
+RUN cp /usr/bin/caddy /staging/caddy
+#prepare heroku app
+ADD ./ /staging/
+RUN chmod +x build/*.sh
+''' + deployCmd
+        }
+        commandLine 'docker', "build", "--pull", "-t", "$rootProject.name:dev", "-f", "$buildDir/Dockerfile", "."
+    }
+}
